46f89762dd9fc33c1af510e1e6685a87547767e3
J Henry <j.henry@actif.ai>
2025-02-25 11:07:07 -0500

    initial bare metal binary setup

---
+++ zero/src/main.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
